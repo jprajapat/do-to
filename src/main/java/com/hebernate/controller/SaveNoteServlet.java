1fa2a88ec70f27a5007481cca647eafdb3e089d5
jitendra prajapat <jprajapat999@gmail.com>
2021-06-25 16:21:09 +0530

    developmet

--- src/main/java/com/hebernate/controller/SaveNoteServlet.java
+++ src/main/java/com/hebernate/controller/SaveNoteServlet.java
@@ -19,8 +19,6 @@ public class SaveNoteServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	public SaveNoteServlet() {
-		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
